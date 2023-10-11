9e7e7e40fcd8315c327e65702c04f177575fdecf
Swetank Subham <swetanksubham.r@gmail.com>
2023-10-11 11:01:18 +0000

    Updated .gitignore and clean image script to keep .gitkeep

--- scripts/clean_images.sh
+++ scripts/clean_images.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
+#!/bin/bash
+
+delete_all_except_gitkeep() {
+    original_dir="$(pwd)"
+
+    # Check if the argument is provided
+    if [ -z "$1" ]; then
+        echo "Usage: delete_all_except_gitkeep <directory_path>"
+        return 1
+    fi
+
+    directory_path="$1"
+
+    # Check if the directory exists
+    if [ -d "$directory_path" ]; then
+        # Change to the directory
+        cd "$directory_path" || return 1
+
+        # Delete all files and directories except .gitkeep
+        find . -mindepth 1 ! -name ".gitkeep" -delete
+        
+        # Change back to the original working directory
+        cd "$original_dir" || return 1
+    else
+        echo "Directory not found: $directory_path"
+        return 1
+    fi
+}
+
 echo "---------- [Starting] ----------"
 
 echo "Clearing Generated Images"
-rm -rf src/main/resources/image/generated_image/*
+delete_all_except_gitkeep src/main/resources/image/generated_image/
 
 echo "Clearing Generated Thumbnails"
-rm -rf src/main/resources/image/generated_thumb/*
+delete_all_except_gitkeep src/main/resources/image/generated_thumb/
 
 echo "---------- [Completed] ----------"
